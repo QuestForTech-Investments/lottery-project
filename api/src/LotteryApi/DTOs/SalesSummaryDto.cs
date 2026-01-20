5aedb9e8c5cba4524fa81d5ef581d97266929ea1
VSJeanLuis <167777381+VSJeanLuis@users.noreply.github.com>
2026-01-20 13:01:22 -0400

    Making sales report summary to return the right data that requires the frontend (#78)

--- api/src/LotteryApi/DTOs/SalesSummaryDto.cs
+++ api/src/LotteryApi/DTOs/SalesSummaryDto.cs
@@ -6,4 +6,7 @@ public class SalesSummaryDto
     public decimal TotalPrizes { get; set; }
     public decimal TotalCommissions { get; set; }
     public decimal TotalNet { get; set; }
+    public decimal BenefitPercentage { get; set; }
+    public decimal Credits { get; set; }
+    public decimal Balance { get; set; }
 }
